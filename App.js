56727e05978bdbed6dc5ebbfa7bca389b75117ef
Jaswin Prakash K <jaswinprakash@gmail.com>
2023-06-30 12:33:28 +0530

    react-native version downgraded for svg loading

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import Home from './src/components/screens/Home'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Home/>
+    </SafeAreaView>
+  )
+}
--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {View, Image, StyleSheet, ScrollView} from 'react-native';
 import React from 'react';
 import QuizCards from './QuizCards';
 import Search from '../../assets/icons/search.svg';
@@ -7,15 +7,19 @@ export default function Home() {
   return (
     <View style={styled.mainContainer}>
       <View style={styled.navbarMain}>
-        <View style={styled.profile}></View>
-        <Search width={50} height={50}/>
+        <Image
+          source={require('../../assets/icons/Logo-p.png')}
+          style={styled.profile}
+        />
+        <View style={styled.searchContainer}>
+          <Search width={25} height={25} style={styled.searchLogo} />
+        </View>
       </View>
       <ScrollView
         contentContainerStyle={styled.filters}
         horizontal={true}
-        showsHorizontalScrollIndicator={false}>
-      </ScrollView>
-      <QuizCards/>
+        showsHorizontalScrollIndicator={false}></ScrollView>
+      <QuizCards />
       <View style={styled.bottomCard}></View>
     </View>
   );
@@ -23,9 +27,18 @@ export default function Home() {
 
 const styled = StyleSheet.create({
   mainContainer: {},
-  navbarMain: {},
-  profile: {},
-  search: {},
+  navbarMain: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal:10,
+  },
+  profile: {
+    height: undefined,
+    width: 150,
+    aspectRatio: 137 / 56,
+  },
+  searchLogo: {},
   filters: {},
   bottomCard: {},
 });
